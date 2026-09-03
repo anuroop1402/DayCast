b3d95b82032e35ffa1521f6e0e2d1fbf02ca77d7
anuroop1402 <203018750+anuroop1402@users.noreply.github.com>
2026-09-03 22:58:03 +0530

    Initial Commit

---
+++ DayCast/DayCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayCastApp.swift
+//  DayCast
+//
+//  Created by Anuroop-Keus on 03/09/26.
+//
+
+import SwiftUI
+
+@main
+struct DayCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
